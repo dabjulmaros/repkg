c1a21fa1d829564f97d5d57269c16387d9d0e000
Dabjulmaros <sampadodegtasa@gmail.com>
2020-09-20 05:50:48 -0400

    v0.5

--- src/render.js
+++ src/render.js
@@ -1,5 +1,6 @@
 const { remote,shell } = require('electron');
 const {dialog,app} = remote;
+const fs = remote.require('fs');
 
 var child_process = require('child_process');
 
@@ -13,12 +14,15 @@ const repkg = document.getElementById('RePKG');
 
 const scenePathSpan = document.getElementById('scenePath');
 const outputPathSpan = document.getElementById('outputPath');
-let scenePath="",outputPath="";
+let scenePath=app.getAppPath(),outputPath=app.getAppPath();
+
+const regex = new RegExp('\(.+)\.tex|/(.+)\.tex', 'g');
 
 //file
 sceneBtn.onclick = e =>{
 	dialog.showOpenDialog({
 		title:"Select scene.pkg",
+		defaultPath:scenePath,
 		filters: [
 			{ name: 'Scene', extensions: ['pkg'] },
 			{ name: 'All Files', extensions: ['*'] }
@@ -36,7 +40,7 @@ sceneBtn.onclick = e =>{
 		sceneBtn.classList.remove('is-warning');
 		sceneBtn.classList.add('is-primary');
 
-		if(outputPath!="")
+		if(outputPath!=app.getAppPath())
 			startBtn.removeAttribute('disabled');
 	}).catch(err => {
 		console.log(err)
@@ -46,6 +50,7 @@ sceneBtn.onclick = e =>{
 outputBtn.onclick = e => {
 	dialog.showOpenDialog({
 		title:"Select Output Folder",
+		defaultPath:outputPath,
 		properties: ['openDirectory']
 	}).then(result => {
 		console.log(result.canceled)
@@ -58,7 +63,7 @@ outputBtn.onclick = e => {
 		outputPathSpan.setAttribute('title', result.filePaths[0]);
 		outputBtn.classList.remove('is-warning');
 		outputBtn.classList.add('is-primary');
-		if(scenePath!="")
+		if(scenePath!=app.getAppPath())
 			startBtn.removeAttribute('disabled');
 	}).catch(err => {
 		console.log(err)
@@ -67,23 +72,71 @@ outputBtn.onclick = e => {
 
 startBtn.onclick= e =>{
 	var child = child_process.exec(`"${app.getAppPath()}\\repkg\\RePKG.exe" extract -e tex -s -o "${outputPath}" "${scenePath}"`);
-	console.log("hello");
+	cmdOutput.innerText="Starting Process";
 	// use event hooks to provide a callback to execute when data are available:
+	let fileNames=[];
 	child.stdout.on('data', function(data) {
-		console.log(data.toString());
+		const cmdout= data.toString();
+		console.log("---"+cmdout+"---");
+		cmdOutput.innerHTML+=`\n${cmdout}`;
+		cmdOutput.scrollTop = cmdOutput.scrollHeight;
+		if(cmdout.match(regex)!=null){
+			files=cmdout.match(regex);
+			for(var file of files){
+				name=file.substr(0,file.length-4).substr(file.lastIndexOf('/')+1).substr(file.lastIndexOf('\\')+1);
+				console.log(name);
+				if(!fileNames.includes(name)){
+					fileNames.push(name);
+			}
+			}
+		}
+		
 	});
 	child.on('exit',e=>	{
 		console.log("done");
+		console.log(fileNames);
+		removeTex(fileNames);
 	});
 	child.on('error',e=>	{
+		cmdOutput.innerHTML+=`\n ERROR! ${data.toString()}`
+		cmdOutput.scrollTop = cmdOutput.scrollHeight;
 		console.log("e");
 	});
 }
 
+function removeTex(fileNames){
+	cmdOutput.innerHTML+=`\n\nRemoving tex and tex-json files.`;
+	cmdOutput.scrollTop = cmdOutput.scrollHeight;
+	fs.readdirSync(outputPath).forEach(file => {
+		for(var x of fileNames){
+			if(file.includes(x+".tex")||file.includes(x+".tex-json")){
+				console.log(file);
+				fs.unlink(`${outputPath}\\${file}`,function(err){
+					if(err){
+						alert("An error ocurred removing the file"+ err.message);
+						console.log(err);
+						shell.beep();
+						outputFolder.parentElement.style.visibility="visible";
+						return;
+					}
+				});
+				cmdOutput.innerHTML+=`\n${file} was removed.`;
+				cmdOutput.scrollTop = cmdOutput.scrollHeight;
+			}
+		}
+	});
 
-outputFolder.on('click',e=>{
+	cmdOutput.innerHTML+=`\n\nCompleted!`;
+	cmdOutput.scrollTop = cmdOutput.scrollHeight;
+	shell.beep();
+	outputFolder.parentElement.style.visibility="visible";
+		
+}
 
+
+outputFolder.addEventListener('click',e=>{
+	shell.openPath(outputPath);
 })
-repkg.on('click',e=>{
-
+repkg.addEventListener('click',e=>{
+	shell.openExternal('https://github.com/notscuffed/repkg');
 })
